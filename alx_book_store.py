fdab6c28bea268efcd5f4eac9f0453b527afd7f0
Hagos Chekole <Hagochek06@gmail.com>
2024-07-27 21:41:24 +0300

    first commit

--- alx_book_store.py
+++ alx_book_store.py
@@ -7,5 +7,22 @@ mydb = mysql.connector.connect(
     password="Do!mybest2121",
     database="alx_book_store"
 )
+mycursor=mydb.cursor()
+# mycursor.execute("SELECT * FROM Authors")
+# for i in mycursor:
+#     print(i)
 
-print(mydb.get_server_info())
+# def tableinsert(author_id,author_name,book_id):
+#     query="INSERT INTO Authors(author_id,author_name,book_id)values(%s,%s,%s)"
+#     values=(author_id,author_name,book_id)
+#     mycursor.execute(query,values)
+#     mydb.commit()
+#     print("Record SuccessFully")
+# tableinsert(1,"kebe",1)
+def books(book_id,title,price,publication_date,author_id):
+    query="INSERT INTO Books(book_id,title,price,publication_date,author_id)values(%s,%s,%s,%s,%s)"
+    values=(book_id,title,price,publication_date,author_id)
+    mycursor.execute(query,values)
+    mydb.commit()
+    print("Successfully inserted into Book table")
+books(1,"Advanced SWE","100,2024-07-26",1)
