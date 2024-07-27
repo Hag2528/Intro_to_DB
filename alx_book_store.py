ee7d12742f7873a6f962eb1ed138dd15e3e59601
Hagos Chekole <Hagochek06@gmail.com>
2024-07-27 23:52:16 +0300

    first commit

--- alx_book_store.py
+++ alx_book_store.py
@@ -12,20 +12,27 @@ mycursor=mydb.cursor()
 # for i in mycursor:
 #     print(i)
 
-# def tableinsert(author_id,author_name,book_id):
-#     query="INSERT INTO Authors(author_id,author_name,book_id)values(%s,%s,%s)"
-#     values=(author_id,author_name,book_id)
-#     mycursor.execute(query,values)
-#     mydb.commit()
-#     print("Record SuccessFully")
-# tableinsert(1,"kebe",1)
-# def books(book_id,title,price,publication_date,author_id):
-#     query="INSERT INTO Books(book_id,title,price,publication_date,author_id)values(%s,%s,%s,%s,%s)"
-#     values=(book_id,title,price,publication_date,author_id)
-#     mycursor.execute(query,values)
-#     mydb.commit()
-#     print("Successfully inserted into Book table")
-# books(1,"Advanced SWE","100","2024-07-26","1")
+def authors(author_id,author_name,book_id):
+    query="INSERT INTO Authors(author_id,author_name,book_id)values(%s,%s,%s)"
+    values=(author_id,author_name,book_id)
+    mycursor.execute(query,values)
+    mydb.commit()
+    print("Record SuccessFully")
+authors(1,"kebe",1)
+def books(book_id,title,price,publication_date,author_id):
+    query="INSERT INTO Books(book_id,title,price,publication_date,author_id)values(%s,%s,%s,%s,%s)"
+    values=(book_id,title,price,publication_date,author_id)
+    mycursor.execute(query,values)
+    mydb.commit()
+    print("Successfully inserted into Book table")
+books(1,"Advanced SWE","100","2024-07-26","1")
+def Customers(customer_id,customer_name,email,address):
+    query2="INSERT INTO Books(customer_id,customer_name,email,address)values(%s,%s,%s,%s)"
+    values=(customer_id,customer_name,email,address)
+    mycursor.execute(query2,values)
+    mydb.commit()
+    print("Successfully inserted into Customers table")
+Customers(1,"Hagos","hag12@gmail.com","addisAbaba")
 def orders(order_id,customer_id,order_date):
     query1="INSERT INTO Books(order_id,customer_id,order_date)values(%s,%s,%s)"
     values=(order_id,customer_id,order_date)
@@ -33,3 +40,14 @@ def orders(order_id,customer_id,order_date):
     mydb.commit()
     print("Successfully inserted into Orders table")
 orders(1,1,"2024-07-26")
+def Order_Details(order_detail_id,order_id,book_id,quantity):
+    query1="INSERT INTO Books(order_detail_id,order_id,book_id,quantity)values(%s,%s,%s,%s)"
+    values=(order_detail_id,order_id,book_id,quantity)
+    mycursor.execute(query1,values)
+    mydb.commit()
+    print("Successfully inserted into Orders table")
+Order_Details(1,1,1,"200")
+    
+    
+    
+    
