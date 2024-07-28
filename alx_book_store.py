8ae1b516bbaa2512a94a9b0988fb09999231a69e
Hagos Chekole <Hagochek06@gmail.com>
2024-07-28 08:27:39 +0300

    first commit

--- alx_book_store.py
+++ alx_book_store.py
@@ -18,29 +18,28 @@ def authors(author_id,author_name,book_id):
     mycursor.execute(query,values)
     mydb.commit()
     print("Record SuccessFully")
-authors(8,"shumuye",1)
+authors(16,"shumuye",1)
 def books(book_id,title,price,publication_date,author_id):
     query="INSERT INTO Books(book_id,title,price,publication_date,author_id)values(%s,%s,%s,%s,%s)"
     values=(book_id,title,price,publication_date,author_id)
     mycursor.execute(query,values)
     mydb.commit()
     print("Successfully inserted into Book table")
-books(8,"Advanced SWE","100","2024-07-26",1)
+books(16,"Advanced SWE","100","2024-07-26",1)
+def Customers(customer_id,customer_name,email,address,order_id):
+    query="INSERT INTO Books(customer_id,customer_name,email,address,order_id)values(%s,%s,%s,%s,%s)"
+    values=(customer_id,customer_name,email,address,order_id)
+    mycursor.execute(query,values)
+    mydb.commit()
+    print("Successfully inserted into Customers table")
+Customers(3,"Hagos","hag12@gmail.com","addisAbaba",1)
 def orders(order_id,customer_id,order_date):
-    query1="INSERT INTO Books(order_id,customer_id,order_date)values(%s,%s,%s)"
+    query="INSERT INTO Books(order_id,customer_id,order_date)values(%s,%s,%s)"
     values=(order_id,customer_id,order_date)
-    mycursor.execute(query1,values)
+    mycursor.execute(query,values)
     mydb.commit()
     print("Successfully inserted into Orders table")
 orders(2,1,"2024-07-26")
-def Customers(customer_id,customer_name,email,address):
-    query2="INSERT INTO Books(customer_id,customer_name,email,address)values(%s,%s,%s,%s)"
-    values=(customer_id,customer_name,email,address)
-    mycursor.execute(query2,values)
-    mydb.commit()
-    print("Successfully inserted into Customers table")
-Customers(1,"Hagos","hag12@gmail.com","addisAbaba")
-
 def Order_Details(order_detail_id,order_id,book_id,quantity):
     query1="INSERT INTO Books(order_detail_id,order_id,book_id,quantity)values(%s,%s,%s,%s)"
     values=(order_detail_id,order_id,book_id,quantity)
