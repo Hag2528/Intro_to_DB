8dcdbcd2d3a03e3eac7759ebe3d2600a9262b234
Hag2528 <hagochek53@gmail.com>
2024-07-28 23:34:56 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,15 +1,15 @@
 import mysql.connector
-def create_database(db_name="alx_book_store", host="localhost", user="your_username", password="your_password"):
+def create_database(db_name="alx_book_store", host="localhost", user="root", password="Do!mybest2121"):
   try:
     connection = mysql.connector.connect(host=host, user=user, password=password)
     cursor = connection.cursor()
   
-    sql = f"CREATE DATABASE IF NOT EXISTS {db_name}"
+    sql = f"CREATE DATABASE IF NOT EXISTS {alx_book_store}"
     cursor.execute(sql)
 
     connection.commit()
 
-    print(f"Database '{db_name}' created successfully!")
+    print(f"Database '{alx_book_store}' created successfully!")
 
   except mysql.connector.Error as err:
     print(f"Error creating database: {err}")
